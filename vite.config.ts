227b00c4769d65482deac9e43b0c0c4ddf60f827
pzhdv <pzh1939673715@gmail.com>
2025-06-28 13:03:39 +0800

    feat(custom): 配置@别名

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // 或者指定为你的局域网 IP 地址
+    port: 4000, // 可选，指定端口号
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+    //导入时想要省略的扩展名列表
+    extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json'],
+  },
 })
