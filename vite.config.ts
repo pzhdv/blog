64cf2710520c6b8c5bfd8b70e4ba02a8a38efd0d
pzhdv <pzh1939673715@gmail.com>
2025-06-28 13:36:07 +0800

    feat(custom): 添加tailwindcss

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig, loadEnv } from 'vite'
 import type { ConfigEnv } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
 // https://vite.dev/config/
@@ -12,7 +13,7 @@ export default defineConfig(({ command, mode }: ConfigEnv) => {
   const { VITE_API_BASE_URL } = env //不同模式中取值
   console.log('VITE_API_BASE_URL', VITE_API_BASE_URL)
   return {
-    plugins: [react()],
+    plugins: [react(), tailwindcss()],
     server: {
       host: '0.0.0.0', // 或者指定为你的局域网 IP 地址
       port: 4000, // 可选，指定端口号
