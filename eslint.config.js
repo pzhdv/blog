ab0adb7a635c5347f609603be8506c9c685777e6
pzhdv <pzh1939673715@gmail.com>
2025-06-28 12:39:17 +0800

    配置eslint、prettier约束代码规范

--- eslint.config.js
+++ eslint.config.js
@@ -6,9 +6,13 @@ import tseslint from 'typescript-eslint'
 import { globalIgnores } from 'eslint/config'
 
 export default tseslint.config([
-  globalIgnores(['dist']),
+  globalIgnores([
+    'dist', // 构建输出目录 忽略整个 dist 目录及其目录下所有内容
+    'node_modules', // Node.js 依赖项
+    'assets/fonts', // 不需要检查的字体资源
+  ]),
   {
-    files: ['**/*.{ts,tsx}'],
+    files: ['**/*.{js,ts,tsx,jsx}'], // 指定 ESLint 检查的文件
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
@@ -18,6 +22,16 @@ export default tseslint.config([
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+    },
+    rules: {
+      /*
+       * 规则严重级别：
+       * "off" 或 0    ==>  关闭规则
+       * "warn" 或 1   ==>  将规则违反显示为警告（代码仍可运行）
+       * "error" 或 2  ==>  将规则违反显示为错误（代码无法运行/编译）
+       */
+      '@typescript-eslint/no-explicit-any': 'off', // 使用any报错
+      '@typescript-eslint/no-unused-vars': 'warn', // 定义未使用报错
     },
   },
 ])
